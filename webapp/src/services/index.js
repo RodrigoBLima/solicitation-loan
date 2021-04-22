4e235d042e488cb9146d545ea1be89f005aae798
Rodrigo Barbosa <vanderdigo837@outlook.com>
2021-04-22 14:58:36 -0300

    env variable

--- webapp/src/services/index.js
+++ webapp/src/services/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: process.env.REACT_APP_API_URL,
-  baseURL:`https://django-api-loan.herokuapp.com`
+  baseURL: process.env.REACT_APP_API_URL,
+
 });
 export default api;
